6f65333872422d0f14cf96075298fd74c5776264
Ilya Yakubov <i.yakubov.pro@gmail.com>
2021-04-27 15:45:10 +0300

    Added a class Util for working with messages and other to the user.

---
+++ src/ui/utils/Util.java
@@ -0,0 +1,25 @@
+package ui.utils;
+
+/**
+ * Вспомогательные методы для работы с консолью
+ */
+public class Util {
+
+    /**
+     * Печатает сообщение
+     *
+     * @param message простая строка для вывода в консоль
+     */
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    /**
+     * Печатает сообщение
+     *
+     * @param message собранная строка типа StringBuilder для вывода в консоль
+     */
+    public static void printMessage(StringBuilder message) {
+        System.out.println(message);
+    }
+}
